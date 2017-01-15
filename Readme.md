ec9de6546081e58a4e9315d2f1c5785abe344bb6
Melissa Cat <missmel@4o4.nl>
2017-01-15 20:49:12 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# playerblips-pro
+
+Playerblips resource for GTA Network servers that provides good-looking blips on the minimap for players on GTA Network servers.
+
+This will provide a numbered blips if there are multiple players in a vehicle, as well as special blip sprites for special vehicles like tanks and jets.
+
+![](Screenshot1.jpg)
+
+![](Screenshot2.jpg)
+
+If you have [`colorednames`](https://github.com/MissyMelissa/colorednames) installed, the player blips will be colored like in the screenshots above.
+
+## Usage
+
+Just install it and it will work.
+
+## Installation
+
+Put the repository as a folder in the `resources` folder and then include it in your config like this:
+
+```XML
+<resource src="playerblips-pro" />
+```
+
+Or start it via the admin resource via: `/start playerblips-pro`
---
+++ playerblips.ts
@@ -0,0 +1,84 @@
+﻿/// <reference path="types-gtanetwork/index.d.ts" />
+
+var g_hashRhino = API.getHashKey("rhino");
+
+var g_hashInsurgent = API.getHashKey("insurgent");
+var g_hashTechnical = API.getHashKey("technical");
+
+var g_hashHydra = API.getHashKey("hydra");
+var g_hashLazer = API.getHashKey("lazer");
+
+function updatePlayerBlip(blip: LocalHandle, player: LocalHandle) {
+	if (blip == null || blip.IsNull) {
+		return;
+	}
+
+	var playerPosition = API.getEntityPosition(player);
+	var playerRotation = API.getEntityRotation(player);
+
+	API.setBlipPosition(blip, playerPosition);
+	API.callNative("SET_BLIP_ROTATION", blip, Math.round(playerRotation.Z));
+}
+
+API.onUpdate.connect(function () {
+	var players = API.getStreamedPlayers();
+
+	for (var i = 0; i < players.Length; i++) {
+		var player: LocalHandle = players[i];
+
+		// WORKAROUND: Ignore if position is reported to be at 0,0,0 exactly.
+		// Bug reported here: https://forum.gtanet.work/index.php?threads/streaming-entities-api-functions-dont-stream-out.1468/
+		var zeroCheck = API.getEntityPosition(player);
+		if (zeroCheck.X == 0 && zeroCheck.Y == 0 && zeroCheck.Z == 0) {
+			continue;
+		}
+
+		var blip: LocalHandle = API.getEntitySyncedData(player, "playerblip");
+		if (blip == null || blip.IsNull) {
+			continue;
+		}
+		updatePlayerBlip(blip, player);
+	}
+
+	var localPlayer: LocalHandle = API.getLocalPlayer();
+	var localPosition = API.getEntityPosition(localPlayer);
+	var localRotation = API.getEntityRotation(localPlayer);
+
+	var localVehicle = API.getPlayerVehicle(localPlayer);
+
+	var localBlip: LocalHandle = API.getEntitySyncedData(localPlayer, "playerblip");
+
+	if (localBlip == null) {
+		return;
+	}
+
+	API.setBlipTransparency(localBlip, 0);
+
+	//var localBlipColor = API.getBlipColor(localBlip); // For testing
+	var localBlipSprite = API.getBlipSprite(localBlip);
+	var newBlipSprite = localBlipSprite;
+
+	updatePlayerBlip(localBlip, localPlayer);
+
+	if (localVehicle.IsNull) {
+		newBlipSprite = 1;
+	} else {
+		var vehicleHash = API.getEntityModel(localVehicle);
+		if (vehicleHash == g_hashRhino) {
+			newBlipSprite = 421;
+		} else if (vehicleHash == g_hashInsurgent || vehicleHash == g_hashTechnical) {
+			newBlipSprite = 460;
+		} else if (vehicleHash == g_hashHydra || vehicleHash == g_hashLazer) {
+			newBlipSprite = 16;
+		} else {
+			newBlipSprite = 1;
+		}
+	}
+
+	if (localBlipSprite != newBlipSprite) {
+		API.setBlipSprite(localBlip, newBlipSprite);
+		//API.setBlipColor(localBlip, localBlipColor); // For testing
+
+		API.triggerServerEvent("playerblip_sprite", newBlipSprite);
+	}
+});
